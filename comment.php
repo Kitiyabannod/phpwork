e1a742deec462b7f88a2e1bb8b96e481cdc48a12
Kitiyabannod <s64309010002@ptnicec.ac.th>
2022-10-21 11:50:48 +0700

    ส่งไฟล์ Comment

---
+++ comment.php
@@ -0,0 +1,13 @@
+<?php 
+
+//บรรทัดที่ 6-7 เเสดงผลการใช้งาน Comment เเบบบรรทัดเดียว
+echo "Kitiya niday";
+echo "Information Technology<br>"; 
+//echo "Pattani<br>";
+#echo "Thailand"; 
+
+//บรรทัดที่ 10-12 เเสดงผลการใช้งาน Comment เเบบบรรทัดเดียว
+/* echo "<br>";
+echo "94000<br>";
+echo "Asia<br>"; */
+echo "Universal";

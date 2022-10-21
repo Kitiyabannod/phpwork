73c1f84086e21b60663d7a73d3ab998a16a6d2d8
Kitiyabannod <s64309010002@ptnicec.ac.th>
2022-10-21 11:28:56 +0700

    ส่งไฟล์ text ครั้งที่1

--- test.php
+++ test.php
@@ -2,4 +2,25 @@
 
 echo "Kitiya Niday";
 
-?>
+?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body> 
+    <br>
+    Information Technology 
+    <br>
+    <?php 
+    echo "Pattani" 
+    ?> 
+    <br> 
+</body>
+</html> 
+<?php
+   echo "Thailand"; 
+   ?>
